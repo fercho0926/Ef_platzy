04f4b194ce4f9f73e16fdf445b410f8b66514e52
Milton Vasquez <milton.vasquez@kineticadvantage.com>
2022-06-13 21:38:32 -0500

    Database Conection

--- Ef_platzy/Program.cs
+++ Ef_platzy/Program.cs
@@ -1,12 +1,20 @@
 using Ef_platzy.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<Ef_PlatzyContex>(p => p.UseInMemoryDatabase("TaskDB"));
+//builder.Services.AddDbContext<Ef_PlatzyContex>(p => p.UseInMemoryDatabase("TaskDB"));
+builder.Services.AddSqlServer<Ef_PlatzyContex>("");
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbConection", async ([FromServices] Ef_PlatzyContex dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Database on memory" + dbContext.Database.IsInMemory());
+});
 
 app.Run();
